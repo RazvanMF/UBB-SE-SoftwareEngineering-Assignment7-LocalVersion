3f219a6f710a400f0feefb8dd8b1bc85fbb0e9e0
Rares Bozga <darwj10011@gmail.com>
2024-06-01 16:00:50 +0300

    Product Page - hopefully works (needs some serious testing) -- ALSO there's TO-DO's of stuff for Dan and Dragos to figure out. Florin should link to this page somehow.

---
+++ Celebration Of Capitalism - The Finale/Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using Celebration_Of_Capitalism___The_Finale.Models;
+using Celebration_Of_Capitalism___The_Finale.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Celebration_Of_Capitalism___The_Finale.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService productService;
+
+        public ProductController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        /**
+         * Shows the Views/[your controller name]/Index.cshtml page
+         * Here Views/Product/Index.cshtml
+         */
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return productService.GetAllProducts();
+        }
+
+        public int AddProduct(Product product)
+        {
+            return productService.AddProduct(product);
+        }
+
+        public bool DeleteProduct(int id)
+        {
+            return productService.DeleteProduct(id);
+        }
+
+        public Product? GetProduct(int id)
+        {
+            return productService.GetProduct(id);
+        }
+
+        public bool UpdateProduct(int id, Product product)
+        {
+            return productService.UpdateProduct(id, product);
+        }
+    }
+}
